15c53275c6677d11a6a87e18b0990b16a4c0008a
Mathew Akre <mwakre29@gmail.com>
2020-04-09 13:13:00 -0700

    make homepage default page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./components/HomePage/index";
 
-function App() {
+import "./App.css";
+
+App = () => {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
